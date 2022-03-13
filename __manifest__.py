c0c8b78d84d4af830c13687a5bf076303295ee53
Tony Galmiche <tony.galmiche@infosaone.com>
2022-03-13 09:23:37 +0100

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name'     : 'InfoSaône - Module Odoo 15 pour J Cloture',
+    'version'  : '0.1',
+    'author'   : 'InfoSaône',
+    'category' : 'InfoSaône',
+    'description': """
+InfoSaône - Module Odoo 15 pour J Cloture
+===================================================
+""",
+    'maintainer' : 'InfoSaône',
+    'website'    : 'http://www.infosaone.com',
+    'depends'    : [
+        'base',
+        'sale_management',
+        'sales_team',
+        'mail',
+        'account',
+        'purchase',
+],
+    'data' : [
+        'report/sale_report_templates.xml',
+        'report/report_invoice.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
+
